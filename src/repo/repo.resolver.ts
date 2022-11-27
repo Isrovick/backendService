00c3065f6858b948b44b2e0e3ff0ac90116b291d
Isrovick <isrovick@gmail.com>
2022-11-27 17:54:33 -0400

    Refactor: 🦾 Adding jwt guard to unprotected resolvers Ref:

--- src/repo/repo.resolver.ts
+++ src/repo/repo.resolver.ts
@@ -2,16 +2,20 @@ import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { RepoService } from './repo.service';
 import { Repo } from './entities/repo.entity';
 import { RepoSearchInput } from './dto/repo-search.input';
+import { UseGuards } from '@nestjs/common';
+import { AuthGQLGuard } from '../auth/jwt.guard';
 
 @Resolver('Repo')
 export class RepoResolver {
   constructor(private readonly repoService: RepoService) {}
 
+  @UseGuards(AuthGQLGuard)
   @Query(() => [Repo])
   findAll(@Args('id') id: number) {
     return this.repoService.findAllByUserId(id);
   }
 
+  @UseGuards(AuthGQLGuard)
   @Mutation(() => Repo)
   setFavourite(@Args('repoSearchInput') repoSearchInput: RepoSearchInput) {
     return this.repoService.setFavourite(repoSearchInput);
